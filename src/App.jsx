2ab21b6fd80c41aad2916797bc7ec6c45f79774b
Your Name <ndimant@hotmail.com>
2023-01-08 14:19:41 +0200

    adding redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import MoviePage from "./pages/Movies";
 import Netflix from "./pages/Netflix";
+import Player from "./pages/Player";
 import Signup from "./pages/Signup";
+import TVShows from "./pages/TVShows";
+import UserListedMovies from "./pages/UserListedMovies";
 
-export default function App(){
-  return(
-<BrowserRouter>
-<Routes>
-  <Route path="/login" element={<Login />}/>
-  <Route path="/signup" element={<Signup />}/>
-  <Route path="/" element={<Netflix />}/>
-</Routes>
-</BrowserRouter>
-  )
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/player" element={<Player />} />
+        <Route path="/tv" element={<TVShows />} />
+        <Route path="/movies" element={<MoviePage />} />
+        <Route path="/new" element={<Player />} />
+        <Route path="/mylist" element={<UserListedMovies />} />
+        <Route path="/" element={<Netflix />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
