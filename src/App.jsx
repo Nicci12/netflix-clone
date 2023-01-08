24ea90d4240e6c0255d12d1497cc759f5f42e94a
Your Name <ndimant@hotmail.com>
2023-01-08 12:33:03 +0200

    firebase setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Netflix from "./pages/Netflix";
+import Signup from "./pages/Signup";
 
 export default function App(){
   return(
-    <div>App</div>
+<BrowserRouter>
+<Routes>
+  <Route path="/login" element={<Login />}/>
+  <Route path="/signup" element={<Signup />}/>
+  <Route path="/" element={<Netflix />}/>
+</Routes>
+</BrowserRouter>
   )
 }
---
+++ src/utils/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/app"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDMDdUdGw8hyaUPVaCkLYEPDGPkAK6deXY",
+  authDomain: "react-netflix-clone-597c4.firebaseapp.com",
+  projectId: "react-netflix-clone-597c4",
+  storageBucket: "react-netflix-clone-597c4.appspot.com",
+  messagingSenderId: "794294374026",
+  appId: "1:794294374026:web:dc0dc007f9a4645615a773",
+  measurementId: "G-XT4V5GCD38"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const firebaseAuth = getAuth(app)
